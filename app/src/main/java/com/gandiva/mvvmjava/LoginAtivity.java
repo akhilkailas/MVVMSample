6560b90bca2bfd80baf248a0998c66c166742104
akhilkailash <akakhil82@gmail.com>
2020-05-10 22:16:02 +0530

    Inital commit

---
+++ app/src/main/java/com/gandiva/mvvmjava/LoginAtivity.java
@@ -0,0 +1,13 @@
+package com.gandiva.mvvmjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginAtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
